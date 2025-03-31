264c0b4d502825db3568ad6e00f9c35e02ab21e8
Keshav Agrawal <keshavagrawal273@gmail.com>
2025-03-31 17:06:16 +0530

    changed dependencies

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+nltk.download('punkt_tab')
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
 ps = PorterStemmer()
 
 
